164e2aaaa395b1f59e44fb7858c0ebeb477efa7a
Enoch Chu <enoch.chu@liferay.com>
2016-01-27 11:16:41 -0800

    Emacs - change to helm-browse-project

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -35,18 +35,18 @@
 
 (global-set-key (kbd "C-1") 'helm-buffers-list)
 (global-set-key (kbd "C-c a") 'ace-jump-mode)
-(global-set-key (kbd "C-c f") 'helm-ls-git-ls)
+(global-set-key (kbd "C-c f") 'helm-browse-project)
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-j") 'move-text-down)
 (global-set-key (kbd "C-k") 'move-text-up)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "TAB") 'self-insert-command)
 (global-set-key [f1] 'neotree-toggle)
-(global-set-key [f8] 'sr-speedbar-toggle)
 (global-set-key [f2] 'whitespace-mode)
 (global-set-key [f3] 'whitespace-cleanup)
 (global-set-key [f4] 'column-enforce-mode)
 (global-set-key [f5] 'sort-lines)
+(global-set-key [f8] 'sr-speedbar-toggle)
 
 (setq fiplr-ignored-globs
 	'((directories (".git" ".svn" "bin"))
