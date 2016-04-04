41be2e71916b86385cb0f35999cbe62245ee7932
Enoch Chu <enoch.chu@liferay.com>
2016-04-04 11:12:01 -0700

    Emacs - Update and remove theme

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -8,8 +8,6 @@
 
 (package-initialize)
 
-(load-theme 'material t)
-
 (autopair-global-mode t)
 (evil-mode 1)
 (global-auto-complete-mode t)
@@ -30,22 +28,23 @@
 (when (eq system-type 'darwin)
 	(set-default-font "Source Code Pro 10"))
 
+(when (eq system-type 'gnu/linux)
+	(set-default-font "Courier 10"))
+
 (global-set-key (kbd "C-1") 'helm-buffers-list)
 (global-set-key (kbd "C-c a") 'ace-jump-mode)
-(global-set-key (kbd "C-c f") 'helm-browse-project)
+(global-set-key (kbd "C-c f") 'fiplr-find-file)
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-j") 'move-text-down)
 (global-set-key (kbd "C-k") 'move-text-up)
-(global-set-key (kbd "M--") 'text-scale-decrease)
-(global-set-key (kbd "M-=") 'text-scale-increase)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "TAB") 'self-insert-command)
 (global-set-key [f1] 'neotree-toggle)
+(global-set-key [f8] 'sr-speedbar-toggle)
 (global-set-key [f2] 'whitespace-mode)
 (global-set-key [f3] 'whitespace-cleanup)
 (global-set-key [f4] 'column-enforce-mode)
 (global-set-key [f5] 'sort-lines)
-(global-set-key [f8] 'sr-speedbar-toggle)
 
 (setq fiplr-ignored-globs
 	'((directories (".git" ".svn" "bin"))
@@ -60,6 +59,6 @@
 		:with-toc nil
 	)))
 
-(defun goto-column (column)
+(defun go-to-column (column)
 	(interactive "nColumn: ")
 	(move-to-column column t))
