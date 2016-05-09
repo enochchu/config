78e7391bba938e781adc5c4b70c3ea629c7bf1d2
Enoch Chu <enoch.chu@liferay.com>
2016-05-09 11:40:39 -0700

    Emacs - Backup

--- emacs/emacs.d/elpa/grails-mode-20151205.2354/grails-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "grails-mode" "20151205.2354" "minor-mode that adds some Grails project management to a grails project" 'nil :url "http://blog.wolfman.com" :keywords '("languages"))
---
+++ emacs/emacs.d/elpa/grails-mode-20160504.211/grails-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "grails-mode" "20160504.211" "minor-mode that adds some Grails project management to a grails project" 'nil :url "http://blog.wolfman.com" :keywords '("languages"))
