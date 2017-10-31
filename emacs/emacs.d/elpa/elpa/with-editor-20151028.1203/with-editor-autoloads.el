b1d9f74caa617b55a18b23d30f8abd087e8ef90d
Enoch Chu <enoch.chu@liferay.com>
2017-10-31 16:50:50 -0700

    Emacs - Delete

--- emacs/emacs.d/elpa/elpa/with-editor-20151028.1203/with-editor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22072 33354 733921 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
--- emacs/emacs.d/elpa/grails-mode-20160504.211/grails-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "grails-mode" "20160504.211" "minor-mode that adds some Grails project management to a grails project" 'nil :url "http://blog.wolfman.com" :keywords '("languages"))
